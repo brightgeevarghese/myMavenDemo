971efec72ccf5b4c73c3665ba937b54e88cfa3a9
Bright Gee Varghese2 <brightlsom@gmail.com>
2024-10-29 12:05:24 -0500

    Second commit

--- src/main/java/edu/miu/cse/Main.java
+++ src/main/java/edu/miu/cse/Main.java
@@ -3,5 +3,6 @@ package edu.miu.cse;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world2!");
     }
 }
